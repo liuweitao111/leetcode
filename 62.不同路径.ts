cc4c14d2f60fb46e5c1ab0a65acd3234d92a2d52
it <2446426696@qq.com>
2020-07-19 09:57:25 +0800

    62.不同路径.ts

---
+++ 62.不同路径.ts
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=typescript
+ *
+ * [62] 不同路径
+ */
+
+// @lc code=start
+function uniquePaths(m: number, n: number): number {
+  const dp:number[][] = Array.from({length: m}).map(v => []);
+  dp[0] = Array(n).fill(1);
+  for(let i = 1; i < m; i++) {
+    dp[i][0] = 1;
+  }
+  for(let i = 1; i < m; i++) {
+    for(let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+  return dp[m - 1][n - 1];
+};
+// @lc code=end
+
