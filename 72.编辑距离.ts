d622761cc5923467eac95a27a94c94fc9b514fc1
it <2446426696@qq.com>
2020-07-20 22:07:09 +0800

    72.编辑距离.ts v2

--- 72.编辑距离.ts
+++ 72.编辑距离.ts
@@ -13,39 +13,22 @@ function minDistance(word1: string, word2: string): number {
     return word1.length;
   }
   const dp: number[][] = [];
-  let flag = true;
-  dp[0] = [1];
-  if(word1[0] === word2[0]) {
-    dp[0][0] = 0;
-    flag = false;
+  for(let i = 0; i < word1.length + 1; i++) {
+    dp[i] = [i]
   }
-  for(let i = 1; i < word1.length; i++) {
-    if(flag && word1[i] === word2[0]) {
-      dp[i] =[dp[i - 1][0]];
-      flag = false;
-    } else {
-      dp[i] = [dp[i - 1][0] + 1];
-    }
+  for(let i = 0; i < word2.length + 1; i++) {
+    dp[0][i] = i;
   }
-  flag = !!dp[0][0];
-  for(let i = 1; i < word2.length; i++) {
-    if(flag && word2[i] === word1[0]) {
-      dp[0][i] = dp[0][i - 1];
-      flag = false;
-    } else {
-      dp[0][i] = dp[0][i - 1] + 1;
-    }
-  }
-  for(let i = 1; i < word1.length; i++) {
-    for(let j = 1; j < word2.length; j++) {
-      if(word1[i] === word2[j]) {
+  for(let i = 1; i < word1.length + 1; i++) {
+    for(let j = 1; j < word2.length + 1; j++) {
+      if(word1[i - 1] === word2[j - 1]) {
         dp[i][j] = dp[i - 1][j - 1];
       } else {
         dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j -1]) + 1;
       }
     }
   }
-  return dp[word1.length - 1][word2.length - 1];
+  return dp[word1.length][word2.length];
 };
 // @lc code=end
 
