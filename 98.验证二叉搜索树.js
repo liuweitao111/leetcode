46c0b15bed95529fd9fd77359660340d71d798c0
it <2446426696@qq.com>
2020-07-30 22:05:20 +0800

    98.验证二叉搜索树.js

---
+++ 98.验证二叉搜索树.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=javascript
+ *
+ * [98] 验证二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+  return helper(root, -Infinity, Infinity);
+};
+function helper(root, lower, upper) {
+  if(!root) {
+    return true;
+  }
+  if(lower >= root.val || upper <= root.val) {
+    return false;
+  }
+  return helper(root.left, lower, root.val) && helper(root.right, root.val, upper);
+}
+// @lc code=end
+
