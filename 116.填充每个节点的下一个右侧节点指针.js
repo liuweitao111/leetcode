1dd3bc2506b701a8c562365eb1d75ea71648aab6
it <2446426696@qq.com>
2020-08-23 22:32:59 +0800

    116.填充每个节点的下一个右侧节点指针.js

---
+++ 116.填充每个节点的下一个右侧节点指针.js
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=116 lang=javascript
+ *
+ * [116] 填充每个节点的下一个右侧节点指针
+ */
+
+// @lc code=start
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) {
+  const queue = [];
+  if(!root) {
+    return root;
+  }
+  queue.push(root);
+  while(queue.length) {
+    const size = queue.length;
+    let pre = null;
+    for(let i = 0; i < size; i++) {
+      const node = queue.shift();
+      if(pre) {
+        pre.next = node;
+      }
+      pre = node;
+      if(node.left) {
+        queue.push(node.left);
+      }
+      if(node.right) {
+        queue.push(node.right);
+      }
+    }
+  }
+  return root;
+};
+// @lc code=end
+
