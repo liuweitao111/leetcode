6d3505268639e3e1f62a5ca47865f24c4376324f
it <2446426696@qq.com>
2020-08-24 22:06:48 +0800

    75.颜色分类.js

--- 75.颜色分类.js
+++ 75.颜色分类.js
@@ -10,16 +10,20 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-  for(let i = 0; i < nums.length; i++) {
-    let minIndex = i;
-    for(let j = i + 1; j < nums.length; j++) {
-      if(nums[minIndex] > nums[j]) {
-        minIndex = j;
-      }
+  let p0 = 0; // 记录0的右边界
+  let p1 = nums.length - 1; // 记录2的左边界
+  let curr = 0;
+  while(curr <= p1) {
+    if(nums[curr] === 0) {
+      [nums[curr], nums[p0]] = [nums[p0], nums[curr]];
+      p0++;
+      curr++;
+    } else if(nums[curr] === 1) {
+      curr++;
+    } else {
+      [nums[curr], nums[p1]] = [nums[p1], nums[curr]];
+      p1--;
     }
-    const tmp = nums[i];
-    nums[i] = nums[minIndex];
-    nums[minIndex] = tmp;
   }
 };
 // @lc code=end
