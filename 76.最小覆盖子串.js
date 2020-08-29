8b79c2a8435204553e9c58b262a89f9704fc3eac
it <2446426696@qq.com>
2020-08-29 11:28:42 +0800

    76.最小覆盖子串.js

--- 76.最小覆盖子串.js
+++ 76.最小覆盖子串.js
@@ -5,44 +5,32 @@
  */
 
 // @lc code=start
+var matches = (needs, window) => Object.keys(needs).every(k => needs[k] <= window[k]);
 /**
  * @param {string} s
  * @param {string} t
  * @return {string}
  */
 var minWindow = function(s, t) {
-  let minLen = Infinity;
-  let resL = 0;
-  const charMap = {};
-  let charType = 0;
+  const needs = {};
+  // 初始化
   for(const c of t) {
-    if(!charMap[c]) {
-      charMap[c] = 1;
-      charType++;
-    }else {
-      charMap[c]++;
-    }
+    needs[c] = (needs[c] || 0) + 1;
   }
+  let minLen = Infinity;
+  let resL = 0; // 记录最小len时l的值
+  const window = {};
   let l = 0;
   let r = 0;
   while(r < s.length) {
-    if(charMap[s[r]] !== undefined) {
-      charMap[s[r]]--;
-      if(charMap[s[r]] === 0) {
-        charType--;
-      }
-    }
-    while(charType === 0) {
+    window[s[r]] = (window[s[r]] || 0) + 1;
+    // 缩小窗口
+    while(matches(needs, window)) {
       if(r - l + 1 < minLen) {
         minLen = r - l + 1;
         resL = l;
       }
-      if(charMap[s[l]] !== undefined) {
-        charMap[s[l]]++;
-        if(charMap[s[l]] === 1) {
-          charType++;
-        }
-      }
+      window[s[l]]--;
       l++;
     }
     r++;
@@ -52,5 +40,47 @@ var minWindow = function(s, t) {
   }
   return s.substring(resL, resL + minLen);
 };
+// var minWindow = function(s, t) {
+//   let minLen = Infinity;
+//   let resL = 0;
+//   const charMap = {};
+//   let charType = 0;
+//   for(const c of t) {
+//     if(!charMap[c]) {
+//       charMap[c] = 1;
+//       charType++;
+//     }else {
+//       charMap[c]++;
+//     }
+//   }
+//   let l = 0;
+//   let r = 0;
+//   while(r < s.length) {
+//     if(charMap[s[r]] !== undefined) {
+//       charMap[s[r]]--;
+//       if(charMap[s[r]] === 0) {
+//         charType--;
+//       }
+//     }
+//     while(charType === 0) {
+//       if(r - l + 1 < minLen) {
+//         minLen = r - l + 1;
+//         resL = l;
+//       }
+//       if(charMap[s[l]] !== undefined) {
+//         charMap[s[l]]++;
+//         if(charMap[s[l]] === 1) {
+//           charType++;
+//         }
+//       }
+//       l++;
+//     }
+//     r++;
+//   }
+//   if(minLen > s.length) {
+//     return "";
+//   }
+//   return s.substring(resL, resL + minLen);
+// };
 // @lc code=end
 
