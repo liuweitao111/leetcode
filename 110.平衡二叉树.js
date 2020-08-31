b5ce9f3b8b2cbe5b519146fe1aecf5e9cfd84ef2
it <2446426696@qq.com>
2020-08-31 22:11:11 +0800

    110.平衡二叉树.js

---
+++ 110.平衡二叉树.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=javascript
+ *
+ * [110] 平衡二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+  const helper = node => {
+    if(!node) {
+      return 0;
+    }
+    const l = helper(node.left);
+    const r = helper(node.right);
+    if(l === - 1 || r === -1 || Math.abs(l - r) > 1) {
+      return -1;
+    }
+    return Math.max(l, r) + 1;
+  }
+  return helper(root) !== -1;
+};
+// @lc code=end
+
