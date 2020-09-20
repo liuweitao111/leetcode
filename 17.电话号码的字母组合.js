53c91715f2ba004dcff628a855a1936b5173ba8b
it <2446426696@qq.com>
2020-09-20 10:00:32 +0800

    17.电话号码的字母组合.js

--- 17.电话号码的字母组合.js
+++ 17.电话号码的字母组合.js
@@ -9,30 +9,51 @@
  * @param {string} digits
  * @return {string[]}
  */
+// var letterCombinations = function(digits) {
+//   const map = {
+//     2: ['a', 'b', 'c'],
+//     3: ['d', 'e', 'f'],
+//     4: ['g', 'h', 'i'],
+//     5: ['j', 'k', 'l'],
+//     6: ['m', 'n', 'o'],
+//     7: ['p', 'q', 'r', 's'],
+//     8: ['t', 'u', 'v'],
+//     9: ['w', 'x', 'y', 'z'],
+//   }
+//   const helper = (i, combinations) => {
+//     if(i === digits.length) {
+//       return combinations;
+//     }
+//     let result = map[digits[i]];
+//     if(i > 0) {
+//       result = result.reduce((res, c) => {
+//         return res.concat(combinations.map(it => it.concat(c)));
+//       }, []);
+//     }
+//     return helper(i + 1, result);
+//   }
+//   return helper(0, []);
+// };
 var letterCombinations = function(digits) {
-  const map = {
-    2: ['a', 'b', 'c'],
-    3: ['d', 'e', 'f'],
-    4: ['g', 'h', 'i'],
-    5: ['j', 'k', 'l'],
-    6: ['m', 'n', 'o'],
-    7: ['p', 'q', 'r', 's'],
-    8: ['t', 'u', 'v'],
-    9: ['w', 'x', 'y', 'z'],
+  if(digits.length === 0) {
+    return [];
   }
-  const helper = (i, combinations) => {
+  const map = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
+  const helper = (combinations, i, combination) => {
     if(i === digits.length) {
-      return combinations;
+      combinations.push(combination);
+      return;
     }
-    let result = map[digits[i]];
-    if(i > 0) {
-      result = result.reduce((res, c) => {
-        return res.concat(combinations.map(it => it.concat(c)));
-      }, []);
+    const letters = map[digits[i]];
+    for(l of letters) {
+      combination = combination.concat(l);
+      helper(combinations, i + 1, combination);
+      combination = combination.slice(0, -1);
     }
-    return helper(i + 1, result);
   }
-  return helper(0, []);
+  const result = [];
+  helper(result, 0, '');
+  return result;
 };
 // @lc code=end
 
