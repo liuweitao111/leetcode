8dffcc07eb38b914abbaa0b3800918857b704aa9
it <2446426696@qq.com>
2020-09-21 09:52:50 +0800

    206.反转链表.js

--- 206.反转链表.js
+++ 206.反转链表.js
@@ -16,14 +16,27 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+// var reverseList = function(head) {
+//   if(!head || !head.next) {
+//     return head;
+//   }
+//   const p = reverseList(head.next);
+//   head.next.next = head;
+//   head.next = null;
+//   return p;
+// };
 var reverseList = function(head) {
-  if(!head || !head.next) {
-    return head;
+  let prev = null;
+  while(head) {
+    // 保存当前head.Next节点，防止重新赋值后被覆盖
+    // 一轮之后状态：nil<-1 2->3->4
+    //              prev   head
+    const temp = head.next;
+    head.next = prev;
+    prev = head;
+    head = temp;
   }
-  const p = reverseList(head.next);
-  head.next.next = head;
-  head.next = null;
-  return p;
+  return prev;
 };
 // @lc code=end
 
