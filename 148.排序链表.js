359b5a95ba69f9414a6b7ed5f1e7dcdb72b28943
it <2446426696@qq.com>
2020-09-22 09:13:00 +0800

    148.排序链表.js

--- 148.排序链表.js
+++ 148.排序链表.js
@@ -20,22 +20,21 @@
 var sortList = function(head) {
   let h = head;
   let length = 0;
-  let interval = 1;
   // 统计长度
   while(h !== null) {
     h = h.next;
     length++;
   }
   //设置头指针，保存head
-  const result = new ListNode(); 
-  result.next = head;
+  const root = new ListNode(); 
+  root.next = head;
   // 依次增加归并排序归并的链表长度
-  while(interval < length) {
-    let preNode = result;
-    h = result.next;
+  for(let seg = 1; seg < length; seg *= 2) {
+    let temp = root;
+    h = root.next;
     while(h !== null) {
       // 切割出第一段有序链表，链表的头部保存的到h1
-      let i = interval;
+      let i = seg;
       let h1 = h; 
       while(i > 0 && h !== null) {
         i--;
@@ -46,38 +45,97 @@ var sortList = function(head) {
         break;
       }
       // 切割出第二段有序链表，链表的头部保存的到h2
-      i = interval;
+      i = seg;
       let h2 = h; 
       while(i > 0 && h !== null) {
         i--;
         h = h.next;
       }
-      let l1 = interval; // 第一段有序链表的长度
-      let l2 = interval - i; // 第二段有序链表的长度
+      let l1 = seg; // 第一段有序链表的长度
+      let l2 = seg - i; // 第二段有序链表的长度
       // 合并两段有序链表
       while(l1 > 0 && l2 > 0) {
         if(h1.val < h2.val) {
-          preNode.next = h1;
+          temp.next = h1;
           h1 = h1.next;
           l1--;
         } else {
-          preNode.next = h2;
+          temp.next = h2;
           h2 = h2.next;
           l2--;
         }
-        preNode = preNode.next;
+        temp = temp.next;
       }
-      preNode.next = l1 === 0 ? h2 : h1;
+      temp.next = l1 === 0 ? h2 : h1;
       while(l1 > 0 || l2 > 0) {
-        preNode = preNode.next;
+        temp = temp.next;
         l1--;
         l2--;
       }
-      preNode.next = h;
+      temp.next = h;
     }
-    interval *= 2;
   }
-  return result.next;
+  return root.next;
 };
+// var sortList = function(head) {
+//   const getLen = () => {
+//     let len = 0;
+//     let next = head;
+//     while(next) {
+//       len++;
+//       next = next.next;
+//     }
+//     return len;
+//   }
+//   const len = getLen();
+//   const root = new ListNode();
+//   for(let seg = 1; seg < len; seg *= 2) {
+//     let next = head;
+//     let temp = root;
+//     while(next) {
+//       let start1 = next;
+//       let i = 0;
+//       while(next && i < seg) {
+//         next = next.next;
+//         i++;
+//       }
+//       let end1 = i;
+//       i = 0;
+//       let start2 = next;
+//       while(next && i < seg) {
+//         next = next.next;
+//         i++;
+//       }
+//       let end2 = i;
+//       while(end1 && end2) {
+//         if(start1.val > start2.val) {
+//           temp.next = start2;
+//           start2 = start2.next;
+//           end2--;
+//         } else {
+//           temp.next = start1;
+//           start1 = start1.next;
+//           end1--;
+//         }
+//         temp = temp.next;
+//       }
+//       while(end1) {
+//         temp.next = start1;
+//         start1 = start1.next;
+//         end1--;
+//         temp = temp.next;
+//       }
+//       while(end2) {
+//         temp.next = start2;
+//         start2 = start2.next;
+//         end2--;
+//         temp = temp.next;
+//       }
+//     }
+//     temp.next = null;
+//     head = root.next;
+//   }
+//   return head;
+// };
 // @lc code=end
 
