49a80ce06ea58d27f384087d818c8cd4d867d098
it <2446426696@qq.com>
2020-09-28 09:24:32 +0800

    142.环形链表-ii.js

--- 142.环形链表-ii.js
+++ 142.环形链表-ii.js
@@ -18,21 +18,44 @@
  * @return {ListNode}
  */
 var detectCycle = function(head) {
-    let pre = head;
-    let fast = head;
-    let slow = head;
-    while(fast && fast.next) {
-      fast = fast.next.next;
-      slow = slow.next;
-      if(fast === slow) {
-        while(pre !== slow) {
-          pre = pre.next;
-          slow = slow.next;
-        }
-        return slow;
-      }
-    }
+  // 思路：快慢指针，快慢相遇之后，慢指针回到头，快慢指针步调一致一起移动，相遇点即为入环点
+  if(!head) {
     return null;
+  }
+  let fast = head.next;
+  let slow = head;
+  while(fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+    if(fast === slow) {
+      // 慢指针重新从头开始移动，快指针从第一次相交点下一个节点开始移动
+      fast = head
+      slow = slow.next // 注意
+      while(fast !== slow) {
+        fast = fast.next;
+        slow = slow.next;
+      }
+      return slow;
+    }
+  }
+  return null;
 };
+// var detectCycle = function(head) {
+//   let pre = head;
+//   let fast = head;
+//   let slow = head;
+//   while(fast && fast.next) {
+//     fast = fast.next.next;
+//     slow = slow.next;
+//     if(fast === slow) {
+//       while(pre !== slow) {
+//         pre = pre.next;
+//         slow = slow.next;
+//       }
+//       return slow;
+//     }
+//   }
+//   return null;
+// };
 // @lc code=end
 
