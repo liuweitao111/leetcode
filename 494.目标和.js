9e33526231f801f5fb0d3993f0132d33b3c9cfbb
it <2446426696@qq.com>
2020-10-16 15:48:26 +0800

    494.目标和.js

--- 494.目标和.js
+++ 494.目标和.js
@@ -10,18 +10,38 @@
  * @param {number} S
  * @return {number}
  */
+// var findTargetSumWays = function(nums, S) {
+//   let result = 0;
+//   const dfs = (i, sum) => {
+//     if(i === nums.length && S === sum) {
+//       result++;
+//     } else if(i < nums.length) {
+//       dfs(i + 1, sum + nums[i]);
+//       dfs(i + 1, sum - nums[i]);
+//     }
+//   }
+//   dfs(0, 0);
+//   return result;
+// };
+// 将加分到A组，将减分到B组 
+// sum(A) - sum(B) = S => 2sum(A) = S + sum(A) + sum(B) sum(A) = (S + sum(nums)) / 2
 var findTargetSumWays = function(nums, S) {
-  let result = 0;
-  const dfs = (i, sum) => {
-    if(i === nums.length && S === sum) {
-      result++;
-    } else if(i < nums.length) {
-      dfs(i + 1, sum + nums[i]);
-      dfs(i + 1, sum - nums[i]);
+  const sum = nums.reduce((total, num) => total + num, 0);
+  if(sum < Math.abs(S)) {
+    return 0;
+  }
+  const target = (sum + S) / 2;
+  if(target - parseInt(target, 10) !== 0) {
+    return 0;
+  }
+  const dp = Array(target + 1).fill(0);
+  dp[0] = 1;
+  for(let i = 0; i < nums.length; i++) {
+    for(let j = target; j >= nums[i]; j--) {
+      dp[j] += dp[j - nums[i]];
     }
   }
-  dfs(0, 0);
-  return result;
+  return dp[target];
 };
 // @lc code=end
 
