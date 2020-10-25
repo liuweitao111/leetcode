17df9e54f4833471b033222c253078d2008ab4b4
it <2446426696@qq.com>
2020-10-25 15:25:05 +0800

    322.零钱兑换.js

--- 322.零钱兑换.js
+++ 322.零钱兑换.js
@@ -10,26 +10,39 @@
  * @param {number} amount
  * @return {number}
  */
+// var coinChange = function(coins, amount) {
+//   if(amount === 0) {
+//     return 0;
+//   }
+//   const dp = [];
+//   for(c of coins) {
+//     dp[c] = 1;
+//   }
+//   for(let i = 1; i <= amount; i++) {
+//     let min = Infinity;
+//     for(c of coins) {
+//       if(i - c > 0 && dp[i - c] && min > dp[i - c]) {
+//         min = dp[i - c];
+//       }
+//     }
+//     if(isFinite(min) && (!dp[i] || dp[i] > min + 1)) {
+//       dp[i] = min + 1;
+//     }
+//   }
+//   return dp[amount] || -1;
+// };
 var coinChange = function(coins, amount) {
-  if(amount === 0) {
-    return 0;
-  }
-  const dp = [];
-  for(c of coins) {
-    dp[c] = 1;
-  }
-  for(let i = 1; i <= amount; i++) {
-    let min = Infinity;
-    for(c of coins) {
-      if(i - c > 0 && dp[i - c] && min > dp[i - c]) {
-        min = dp[i - c];
+  const len = amount + 1;
+  const dp = Array(len).fill(len);
+  dp[0] = 0;
+  for(let i = 1; i < len; i++) {
+    for(const coin of coins) {
+      if(i - coin >= 0) {
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
       }
     }
-    if(isFinite(min) && (!dp[i] || dp[i] > min + 1)) {
-      dp[i] = min + 1;
-    }
   }
-  return dp[amount] || -1;
-};
+  return dp[amount] > amount ? -1 : dp[amount];
+}
 // @lc code=end
 
