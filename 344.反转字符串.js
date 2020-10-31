96011a134fd72e56a3932578ed2616a0a95b77c3
it <2446426696@qq.com>
2020-10-31 12:26:33 +0800

    344.反转字符串.js

--- 344.反转字符串.js
+++ 344.反转字符串.js
@@ -10,13 +10,25 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-  let left = 0;
-  let right = s.length - 1;
-  while(left < right) {
-    [s[left], s[right]] = [s[right], s[left]];
-    left++;
-    right--;
+  const res = reverse(s, 0);
+  for(let i = 0; i < res.length; i++) {
+    s[i] = res[i];
   }
 };
+function reverse(s, i) {
+  if(i === s.length) {
+    return '';
+  }
+  return reverse(s, i + 1) + s[i];
+}
+// var reverseString = function(s) {
+//   let left = 0;
+//   let right = s.length - 1;
+//   while(left < right) {
+//     [s[left], s[right]] = [s[right], s[left]];
+//     left++;
+//     right--;
+//   }
+// };
 // @lc code=end
 
