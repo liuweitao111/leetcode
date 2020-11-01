fd9f7cf001163f01d8f920ce399a1fca4f2913a3
it <2446426696@qq.com>
2020-11-01 22:07:50 +0800

    24.两两交换链表中的节点.js

--- 24.两两交换链表中的节点.js
+++ 24.两两交换链表中的节点.js
@@ -16,24 +16,39 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-  const root = new ListNode();
-  root.next = head;
-  let h = root;
-  while(h) {
-    const h1 = h.next;
-    if(!h1) {
-      break;
-    }
-    const h2 = h1.next;
-    if(!h2) {
-      break;
-    }
-    h.next = h1.next;
-    h1.next = h2.next;
-    h2.next = h1;
-    h = h1;
+  // 思路：将链表翻转转化为一个子问题，然后通过递归方式依次解决
+  // 先翻转两个，然后将后面的节点继续这样翻转，然后将这些翻转后的节点连接起来
+  return helper(head);
+}
+function helper(head) {
+  if(!head || !head.next) {
+    return head;
   }
-  return root.next;
-};
+  const nextHead = head.next.next;
+  const next = head.next;
+  next.next = head;
+  head.next = helper(nextHead);
+  return next;
+}
+// var swapPairs = function(head) {
+//   const root = new ListNode();
+//   root.next = head;
+//   let h = root;
+//   while(h) {
+//     const h1 = h.next;
+//     if(!h1) {
+//       break;
+//     }
+//     const h2 = h1.next;
+//     if(!h2) {
+//       break;
+//     }
+//     h.next = h1.next;
+//     h1.next = h2.next;
+//     h2.next = h1;
+//     h = h1;
+//   }
+//   return root.next;
+// };
 // @lc code=end
 
