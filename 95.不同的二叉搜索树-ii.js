198eb1291709c34e804007ec6970f660d8d20077
it <2446426696@qq.com>
2020-11-02 09:20:47 +0800

    95.不同的二叉搜索树-ii.js

--- 95.不同的二叉搜索树-ii.js
+++ 95.不同的二叉搜索树-ii.js
@@ -18,26 +18,49 @@
  * @return {TreeNode[]}
  */
 var generateTrees = function(n) {
-  const helper = (start, end) => {
-    if(start > end) {
-      return [null];
-    }
-    const res = [];
-    for(let i = start; i <= end; i++) {
-      const l = helper(start, i - 1);
-      const r = helper(i + 1, end);
-      l.forEach(lRes => {
-        r.forEach(rRes => {
-          res.push(new TreeNode(i, lRes, rRes))
-        })
-      });
-    }
-    return res;
-  };
   if(n === 0) {
     return [];
   }
-  return helper(1, n);
+  return generate(1, n);
 };
+
+function generate(start, end) {
+  if(start > end) {
+    return [null];
+  }
+  const res = [];
+  for(let i = start; i <= end; i++) {
+    const left = generate(start, i - 1);
+    const right = generate(i + 1, end);
+    for(let j = 0; j < left.length; j++) {
+      for(let k = 0; k < right.length; k++) {
+        res.push(new TreeNode(i, left[j], right[k]));
+      }
+    }
+  }
+  return res;
+}
+// var generateTrees = function(n) {
+//   const helper = (start, end) => {
+//     if(start > end) {
+//       return [null];
+//     }
+//     const res = [];
+//     for(let i = start; i <= end; i++) {
+//       const l = helper(start, i - 1);
+//       const r = helper(i + 1, end);
+//       l.forEach(lRes => {
+//         r.forEach(rRes => {
+//           res.push(new TreeNode(i, lRes, rRes))
+//         })
+//       });
+//     }
+//     return res;
+//   };
+//   if(n === 0) {
+//     return [];
+//   }
+//   return helper(1, n);
+// };
 // @lc code=end
 
