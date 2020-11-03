b993f24b147cfb980b36590be81f9d6c536a1944
it <2446426696@qq.com>
2020-11-03 09:23:33 +0800

    509.斐波那契数.js

--- 509.斐波那契数.js
+++ 509.斐波那契数.js
@@ -5,20 +5,32 @@
  */
 
 // @lc code=start
-const map = {};
 /**
  * @param {number} N
  * @return {number}
  */
+// 递归 + 备忘录
+// const map = {};
+// var fib = function(N) {
+//   if(N < 2) {
+//     return N;
+//   }
+//   if(map[N]) {
+//     return map[N];
+//   }
+//   map[N] = fib(N - 1) + fib(N - 2);
+//   return map[N];
+// };
+
+尾递归
 var fib = function(N) {
-  if(N < 2) {
-    return N;
-  }
-  if(map[N]) {
-    return map[N];
-  }
-  map[N] = fib(N - 1) + fib(N - 2);
-  return map[N];
+  return helper(N, 0, 1);
 };
+function helper(n, prev, curr) {
+  if(n === 0) {
+    return prev;
+  }
+  return helper(n - 1, curr, curr + prev);
+}
 // @lc code=end
 
