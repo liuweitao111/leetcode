0a514674e0250ced667e899adc0b2e01c8f4d572
it <2446426696@qq.com>
2020-11-18 08:58:49 +0800

    110.平衡二叉树.js

--- 110.平衡二叉树.js
+++ 110.平衡二叉树.js
@@ -23,12 +23,26 @@ var isBalanced = function(root) {
     }
     const l = helper(node.left);
     const r = helper(node.right);
-    if(l === - 1 || r === -1 || Math.abs(l - r) > 1) {
+    if(l === -1 || r === -1 || Math.abs(l - r) > 1) {
       return -1;
     }
     return Math.max(l, r) + 1;
   }
   return helper(root) !== -1;
 };
+// var isBalanced = function(root) {
+//   const helper = node => {
+//     if(!node) {
+//       return 0;
+//     }
+//     const l = helper(node.left);
+//     const r = helper(node.right);
+//     if(l === - 1 || r === -1 || Math.abs(l - r) > 1) {
+//       return -1;
+//     }
+//     return Math.max(l, r) + 1;
+//   }
+//   return helper(root) !== -1;
+// };
 // @lc code=end
 
