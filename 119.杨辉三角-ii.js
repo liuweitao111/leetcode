83626d437eaaedac99f8e5a51ffe0a9ffb6eaf77
it <2446426696@qq.com>
2020-11-19 08:49:30 +0800

    119.杨辉三角-ii.js

--- 119.杨辉三角-ii.js
+++ 119.杨辉三角-ii.js
@@ -10,23 +10,15 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
-  const C = (n, m) => {
-    if(!m || m === n) {
-      return 1;
-    }
-    if(m > n >> 1) {
-      m = n - m;
-    }
-    let res = 1;
-    for(let i = 1; i <= m; i++) {
-      res *= (n - i + 1);
-      res /= i;
-    }
-    return res;
-  }
   const res = [];
-  for(let i = 0; i <= rowIndex; i++) {
-    res[i] = C(rowIndex, i);
+  res[0] = 1;
+  res[rowIndex] = 1;
+  // 左右对称
+  let i = 1;
+  while(rowIndex >= i << 1) {
+    res[i] = res[i - 1] * (rowIndex - i + 1) / i;
+    res[rowIndex - i] = res[i];
+    i++;
   }
   return res;
 };
