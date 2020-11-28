ca97d3e37da3f5fa60df7d3630a47ce3905e4664
it <2446426696@qq.com>
2020-11-28 10:38:11 +0800

    70.爬楼梯.js

--- 70.爬楼梯.js
+++ 70.爬楼梯.js
@@ -9,14 +9,24 @@
  * @param {number} n
  * @return {number}
  */
+// var climbStairs = function(n) {
+//   const helper = (n, prev1, prev2) => {
+//     if(n === 0) {
+//       return prev1;
+//     }
+//     return helper(n - 1, prev2, prev2 + prev1);
+//   }
+//   return helper(n , 1, 1);
+// };
 var climbStairs = function(n) {
-  const helper = (n, prev1, prev2) => {
-    if(n === 0) {
-      return prev1;
-    }
-    return helper(n - 1, prev2, prev2 + prev1);
+  let dp0 = 1;
+  let dp1 = 1;
+  for(let i = 2; i <= n; i++) {
+    const dp2 = dp0 + dp1;
+    dp0 = dp1;
+    dp1 = dp2;
   }
-  return helper(n , 1, 1);
+  return dp1;
 };
 // @lc code=end
 
