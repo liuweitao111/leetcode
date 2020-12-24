509aa8f090a16851fe36ce719b4e0a472df75025
liuweitao.sh <2446426696@qq.com>
2020-12-24 09:14:36 +0800

    151.翻转字符串里的单词.js

---
+++ 151.翻转字符串里的单词.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=151 lang=javascript
+ *
+ * [151] 翻转字符串里的单词
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+  return s.trim().replace(/\s+/g, " ").split(" ").reverse().join(" ");
+};
+// @lc code=end
+
